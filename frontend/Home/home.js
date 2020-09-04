a7c45bd681b9657dcea2d3e41e308be31f25a3a6
Vitor Santos <vitor.santos430@etec.sp.gov.br>
2020-09-04 19:40:48 -0300

    Blink bolado

---
+++ frontend/Home/home.js
@@ -0,0 +1,11 @@
+
+function typeWriter(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
+}
+
+const titulo = document.querySelector('.titulo');
+typeWriter(titulo);
--- frontend/Home/index.php
+++ frontend/Home/index.php
@@ -15,6 +15,7 @@
    
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="frontend/Home/home.css">
 
     <style>
           .bd-placeholder-img {
@@ -65,7 +66,7 @@
         
         <div class="py-5">
             <center>
-                <img src="img/fintato.png" alt="Menu Sticky Fintato!" width="200" id="img_menu_sticky">
+                <h1 class="my-5 text-white titulo">Fintato, você com menos gastos!</h1>
             </center>
         </div>
 
@@ -218,6 +219,7 @@
           </footer>
         </div>
         <script src="bootstrap/js/bootstrap.min.js"></script>
+        <script src="frontend/Home/home.js"></script>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script>
